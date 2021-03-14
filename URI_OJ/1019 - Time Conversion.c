c8ecee480421c81717d82d8aa251cfe6e099ddab
tawhidib <tawhidib@gmail.com>
2021-03-14 12:29:27 +0600

    Create 1019 - Time Conversion.c

---
+++ URI_OJ/1019 - Time Conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int N, hour, minute, second;
+    scanf("%d", &N);
+    hour = (N/3600);
+    minute = ((N - (hour * 3600))/60);
+    second = (N - (hour * 3600) - (minute * 60));
+    printf("%d:%d:%d\n", hour, minute, second);
+    return 0;
+}
