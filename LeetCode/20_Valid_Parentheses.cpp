454097a8f0634f2e7707d2877f769c592e400a85
tawhidib <tawhidib@gmail.com>
2022-11-06 12:22:30 +0600

    LeetCode 20 solved

--- LeetCode/20_Valid_Parentheses.cpp
+++ LeetCode/20_Valid_Parentheses.cpp
@@ -1,51 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-bool isValid(string s) {
-  if(s.length() % 2 == 1) {
-    return false;
-  }
-  /*
+/*
   40 = (
   41 = )
   123 = {
   125 = }
   91 = [
   93 = ]
-  */
+*/
+bool isValid(string s) {
+  if(s.length() % 2 == 1) {
+    return false;
+  }
   stack<char> stk;
   for(int i = 0; i < s.length(); ++i) {
     if(s[i] == 91 || s[i] == 123 || s[i] == 40) {
       stk.push(s[i]);
     }
-    else if(s[i] == 41) {
-      if(stk.top() == 40) {
-        stk.pop();
-        continue;
-      }
-      else {
+    else {
+      if(stk.size() == 0) {
         return false;
       }
-    }
-    else if(s[i] == 125) {
-      if(stk.top() == 123) {
-        stk.pop();
-        continue;
+      else if(s[i] == 41) {
+        if(stk.top() == 40) {
+          stk.pop();
+          continue;
+        }
+        else {
+          return false;
+        }
       }
-      else {
-        return false;
+      else if(s[i] == 125) {
+        if(stk.top() == 123) {
+          stk.pop();
+          continue;
+        }
+        else {
+          return false;
+        }
       }
-    }
-    else if(s[i] == 93) {
-      if(stk.top() == 91) {
-        stk.pop();
-        continue;
-      }
-      else {
-        return false;
+      else if(s[i] == 93) {
+        if(stk.top() == 91) {
+          stk.pop();
+          continue;
+        }
+        else {
+          return false;
+        }
       }
     }
   }
+
   if(stk.size() == 0) {
     return true;
   }
@@ -53,7 +58,7 @@ bool isValid(string s) {
 }
 
 int main(){
-    string s = "(]";
+    string s = "){";
     bool res;
     res = isValid(s);
     printf("%d", res);
