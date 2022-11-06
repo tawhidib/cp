34dbe192792498e7513a435acdb390b8e0d4dc90
tawhidib <tawhidib@gmail.com>
2022-11-06 11:07:34 +0600

    LeetCode 14 solve remain

---
+++ LeetCode/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    vector<char> prefix;
+    char p;
+    for(int i = 0; i < strs.size(); ++i) {
+
+    }
+}
+
+int main(){
+    vector<string> strs{"flower","flow","flight"};
+    longestCommonPrefix(strs);
+    return 0;
+}
