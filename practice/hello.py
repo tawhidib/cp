010ab6748c13480b4f7ab311442cba285f5d1cfd
tawhidib <tawhidib@gmail.com>
2022-11-12 13:39:22 +0600

    start learning python

---
+++ practice/hello.py
@@ -0,0 +1,77 @@
+"""
+print("Hello world!")
+
+if 5 > 3:
+  print("5 is greater than 3")
+
+
+"""
+
+"""
+x = 5
+y = "hello"
+
+print(x + y) // unsupported operand type(s) for +: 'int' and 'str'
+print(str(x) + y)
+
+"""
+
+"""
+
+Variable names are case-sensitive.
+a = 5
+A = 10
+print(a)
+print(A)
+
+"""
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# #convert from int to float:
+# a = float(x)
+
+# #convert from float to int:
+# b = int(y)
+
+# #convert from int to complex:
+# c = complex(x)
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+import random
+
+# print(random.randrange(1, 9999))
+
+# for x in "banana":
+#   print(x)
+
+# txt = "The best things in life are free!"
+# print("z" in txt)
+
+b = "Hello, World!"
+print(b[2:5])
