b36a39894805d4802e5e1beedf9019b68ab7669e
tawhidib <tawhidib@gmail.com>
2022-11-14 19:06:44 +0600

    while loop and break/continue done

---
+++ learning_python/10_while_loop_for_math_table.py
@@ -0,0 +1,7 @@
+table = int(input("Enter which table you want: "))
+i = 1
+while i <= 10:
+  print(f"{i} * {table} = {i * table}")
+  i = i + 1
+
+
---
+++ learning_python/11_while_loop_sum_of_1_to_n.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number: "))
+i = 1
+sum = 0;
+while i <= n:
+  sum = sum + i
+  i = i + 1
+
+print(f"sum of 1 to {n} = {sum}")
