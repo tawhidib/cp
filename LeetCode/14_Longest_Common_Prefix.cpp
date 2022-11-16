e3c3d7c486530552665b35247233d4d456aa96ad
tawhidib <tawhidib@gmail.com>
2022-11-16 22:31:53 +0600

    leetcode 14 getting error

--- LeetCode/14_Longest_Common_Prefix.cpp
+++ LeetCode/14_Longest_Common_Prefix.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string prefixFinder (string str1, string str2) {
+    string s = "";
+    int i = 0;
+    while (str1[i] == str2[i] && str1.length() > 0 && str2.length() > 0)
+    {
+        s = s + str1[i];
+        ++i;
+    }
+    return s;
+}
 string longestCommonPrefix(vector<string>& strs) {
     vector<char> prefix;
     string s = "";
-    string first_str = *(strs.begin());
+    if(strs.size() == 1){ 
+        return *(strs.begin());
+    }
+    int i = 2;
+    s = prefixFinder(*(strs.begin()), *(strs.begin() + 1));
+    while (s.length() > 0 && strs.size() > i)
+    {
+        s = prefixFinder(s, *(strs.begin() + i));
+        ++i;
+    }
+    
+    return s;
 }
 
 int main(){
-    vector<string> strs{"flower","flow","flight"};
-    longestCommonPrefix(strs);
+    vector<string> strs{"",""};
+    string s = longestCommonPrefix(strs);
+    cout << s << endl;
     return 0;
 }
