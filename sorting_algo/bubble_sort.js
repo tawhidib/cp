9fcdc9f5175f3426617183ff8e84b28cfaa10225
tawhidib <tawhidib@gmail.com>
2022-12-26 13:04:24 +0600

    slight improvement on bubble sort

--- sorting_algo/bubble_sort.js
+++ sorting_algo/bubble_sort.js
@@ -2,13 +2,16 @@ const a = [4, 2, 5, 1, 6, 7, 3];
 
 const bubbleSort = (arr) => {
   for (let k = 0; k < arr.length; ++k) {
+    let flag = 0;
     for (let i = 0; i < arr.length - 1 - k; ++i) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
+        flag++;
       }
     }
+    if (flag === 0) break;
   }
 
   return arr;
